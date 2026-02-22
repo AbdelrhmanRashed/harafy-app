a6c0664d1b8d17a0b39bec61a1ec4e866844fb33
AbdelrhmanRashed <abdoe4567@gmail.com>
2026-02-23 01:09:49 +0200

    app structure

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { useState } from 'react';
+
+const text: string = 'ahmed';
+const App = () => {
+  return <div className="my-5 bg-amber-100 p-3 text-amber-900">{text}</div>;
+};
+
+export default App;
