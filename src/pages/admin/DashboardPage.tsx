5d2802b8a04c39eb0fe53862839aa3238a08281e
AbdelrhmanRashed <abdoe4567@gmail.com>
2026-03-28 01:16:19 +0200

    fixes

--- src/pages/admin/DashboardPage.tsx
+++ src/pages/admin/DashboardPage.tsx
@@ -55,9 +55,9 @@ const DashboardPage = () => {
           <StatCard {...stat} key={index} />
         ))}
       </div>
-      <div className="grid gap-4 p-4 md:grid-cols-2">
+      <div className="grid grid-cols-1 gap-4 p-4 xl:grid-cols-2">
         <RequestsChart />
-        <div className="grid gap-4 md:grid-cols-2">
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
           <UserDistributionCard />
           <QuickActionsCard />
         </div>
